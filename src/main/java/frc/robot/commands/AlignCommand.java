2465a974e629e9fa71cb3b5dfa6dde7f699c81f4
RaulRv007 <116015516+RaulRv007@users.noreply.github.com>
2025-02-24 16:01:49 -0500

    -

--- src/main/java/frc/robot/commands/AlignCommand.java
+++ src/main/java/frc/robot/commands/AlignCommand.java
@@ -25,9 +25,9 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.units.measure.*;
-import frc.robot.LimelightHelpers;
-import frc.robot.LimelightHelpers.LimelightTarget_Fiducial;
-import frc.robot.Constants.VisionConstants;
+import frc.robot.utils.LimelightHelpers;
+import frc.robot.utils.LimelightHelpers.LimelightTarget_Fiducial;
+import frc.robot.utils.Constants.VisionConstants;
 import org.json.simple.JSONObject;
 
 import java.io.Console;

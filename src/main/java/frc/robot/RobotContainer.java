85e86a4cd35edd5231106df4ff5e0423004811ba
billy <billy.parmenter98@gmail.com>
2025-03-03 04:35:31 -0500

    added some bindings that might be useful

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,81 +8,66 @@ import static edu.wpi.first.units.Units.*;
 
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
+import com.pathplanner.lib.auto.AutoBuilder;
 import com.ctre.phoenix6.hardware.Pigeon2;
 
-import com.pathplanner.lib.auto.AutoBuilder;
-
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.net.PortForwarder;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.PrintCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
-import frc.robot.commands.AlignCommand;
-import frc.robot.commands.PathToAprilTagCommand;
 import frc.robot.commands.ResetGyro;
 import frc.robot.commands.ZeroPCM;
-import frc.robot.commands.arm.MoveArm;
-import frc.robot.commands.arm.MoveArmToPosition;
-import frc.robot.commands.elevator.MoveElevator;
-import frc.robot.commands.elevator.MoveElevatorToPosition;
-import frc.robot.commands.elevator.MoveElevatorToPositionSDB;
 import frc.robot.commands.climber.MoveClimberToPosition;
-import frc.robot.commands.climber.MoveClimberToPositionJog;
-import frc.robot.commands.climber.MoveClimberToPositionSDB;
 import frc.robot.generated.TunerConstants;
-import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Climber;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Limelight;
 import frc.robot.utils.Telemetry;
 
 public class RobotContainer {
     private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
-    private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second
-                                                                                      // max angular velocity
+    private double MaxAngularRate = RotationsPerSecond.of(0.6).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
 
     // /* Setting up bindings for necessary control of the swerve drive platform */
     private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
-            .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
-            .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
+        .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1); // Add a 10% deadband
     private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
-    // private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
-    private final SwerveRequest.RobotCentric forwardStraight = new SwerveRequest.RobotCentric()
-            .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
+    private final SwerveRequest.RobotCentric forwardStraight = new SwerveRequest.RobotCentric();
+    private final SwerveRequest.FieldCentricFacingAngle facingAngle = new SwerveRequest.FieldCentricFacingAngle();
 
-     private final Telemetry logger = new Telemetry(MaxSpeed);
+    private final Telemetry logger = new Telemetry(MaxSpeed);
 
     private final CommandXboxController joystick = new CommandXboxController(0);
 
-     public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
+    public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
 
-    // public final Limelight seaweed = new Limelight("limelight-seaweed");
- private final Pigeon2 pidgey = new Pigeon2(0, "CANivore"); // Pigeon is on roboRIO CAN Bus with device ID 0
-     //private final ResetGyro resetGyro = new ResetGyro(drivetrain, seaweed, pidgey);
+    public final Limelight seaweed = new Limelight("limelight-seaweed");
+    private final Pigeon2 pidgey = new Pigeon2(0, "CANivore");
 
     // /* Path follower */
-    // private final SendableChooser<Command> autoChooser;
+    private final SendableChooser<Command> autoChooser;
     private final Arm arm = new Arm();
     private final Elevator elevator = new Elevator();
     private final Climber climber = new Climber();
-    // private final Intake intake = new Intake(); 
+    private final Intake intake = new Intake(); 
 
     private double targetPosition = 0.1;
 
     public RobotContainer() {
 
-        // autoChooser = AutoBuilder.buildAutoChooser("Tests");
-        // SmartDashboard.putData("Auto Mode", autoChooser);
-        // pidgey.clearStickyFault_BootDuringEnable();
+        autoChooser = AutoBuilder.buildAutoChooser("Tests");
+        SmartDashboard.putData("Auto Mode", autoChooser);
+        pidgey.clearStickyFault_BootDuringEnable();
         configureDrivetrainBindings();
         configureBindings();
 
@@ -160,21 +145,47 @@ public class RobotContainer {
                 ));
                 
 
-        // joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
-        // joystick.b().whileTrue(drivetrain.applyRequest(
-        // () -> point.withModuleDirection(new Rotation2d(-joystick.getLeftY(),
-        // -joystick.getLeftX()))));
+        joystick.rightBumper().and(joystick.a().whileTrue(drivetrain.applyRequest(() -> brake))
+        );
 
-        // joystick.pov(0).whileTrue(drivetrain.applyRequest(() -> forwardStraight.withVelocityX(0.5).withVelocityY(0)));
-        // joystick.pov(180)
-        //         .whileTrue(drivetrain.applyRequest(() -> forwardStraight.withVelocityX(-0.5).withVelocityY(0)));
+        joystick.rightBumper().and(joystick.b()
+            .whileTrue(
+                new SequentialCommandGroup(
+                    new InstantCommand(() ->
+                        drivetrain.applyRequest(() -> facingAngle.withTargetDirection(Rotation2d.fromDegrees(45)))
+                    ).withTimeout(0.1),
+                    drivetrain.applyRequest(() -> forwardStraight.withVelocityX(-joystick.getLeftY() * MaxSpeed).withVelocityY(-joystick.getLeftX() * MaxSpeed))
+                )
+            )
+        );
 
-        // Run SysId routines when holding back/start and X/Y.
-        // Note that each routine should be run exactly once in a single log.
-        joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
-        joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
-        joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
-        joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+        joystick.rightBumper().and(joystick.povUp()
+            .whileTrue(drivetrain.applyRequest(() -> forwardStraight.withVelocityX(0.5).withVelocityY(0)))
+        );
+        joystick.rightBumper().and(joystick.povUpRight()
+            .whileTrue(drivetrain.applyRequest(() -> forwardStraight.withVelocityX(0.5).withVelocityY(-.5)))
+        );
+        joystick.rightBumper().and(joystick.povUpLeft()
+            .whileTrue(drivetrain.applyRequest(() -> forwardStraight.withVelocityX(0.5).withVelocityY(.5)))
+        );
+
+        joystick.rightBumper().and(joystick.povDown()
+            .whileTrue(drivetrain.applyRequest(() -> forwardStraight.withVelocityX(-0.5).withVelocityY(0)))
+        );
+        joystick.rightBumper().and(joystick.povDownRight()
+            .whileTrue(drivetrain.applyRequest(() -> forwardStraight.withVelocityX(-0.5).withVelocityY(-.5)))
+        );
+        joystick.rightBumper().and(joystick.povDownLeft()
+            .whileTrue(drivetrain.applyRequest(() -> forwardStraight.withVelocityX(-0.5).withVelocityY(.5)))
+        );
+
+
+        joystick.rightBumper().and(joystick.povRight()
+            .whileTrue(drivetrain.applyRequest(() -> forwardStraight.withVelocityX(0).withVelocityY(-0.5)))
+        );
+        joystick.rightBumper().and(joystick.povLeft()
+            .whileTrue(drivetrain.applyRequest(() -> forwardStraight.withVelocityX(0.5).withVelocityY(0)))
+        );
 
         // reset the field-centric heading on left bumper press
         joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
@@ -191,21 +202,11 @@ public class RobotContainer {
 
     public Command getAutonomousCommand() {
         /* Run the path selected from the auto chooser */
-        // return autoChooser.getSelected();
-        return null;
+        return autoChooser.getSelected();
     }
 
     private void adjustTargetPosition(double delta) {
         this.targetPosition += delta;
         SmartDashboard.putNumber("Target Position", targetPosition);
-        
-        // If the motor is currently moving, update the target immediately
-        // if (joystick.rightBumper().getAsBoolean()) {
-        //     climber.positionControlledMotor.goToPosition(this.targetPosition);
-        // }
-    }
-
-    private double getTargetPosition() {
-        return this.targetPosition;
     }
 }

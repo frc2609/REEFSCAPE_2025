958bca0fc298cd306952b0d8edd40d5cacfbb5fe
billy <billy.parmenter98@gmail.com>
2025-03-03 04:36:23 -0500

    Fixed formatting

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -8,19 +8,22 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
     private final SparkMax motor;
+    
     public Intake() {
         motor = new SparkMax(9, MotorType.kBrushless);
         SparkMaxConfig motorConfig = new SparkMaxConfig();
         motorConfig.inverted(false);
-motor.configure(motorConfig, null, null);
+        motor.configure(motorConfig, null, null);
         motor.set(0);
 
-     } 
-public void setSpeed(double speed) {
-    motor.set(speed);
+    } 
+
+    public void setSpeed(double speed) {
+        motor.set(speed);
     }
-public void stop() {
-    motor.set(0);
+
+    public void stop() {
+        motor.set(0);
     }
 
 

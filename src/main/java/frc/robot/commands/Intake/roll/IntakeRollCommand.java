aae2496075b34f611a46505a793fff6a4666dc99
billyparmenter <bparmenter6387@conestogac.on.ca>
2025-03-05 01:45:44 -0500

    Created and renamed and moved roll commands

---
+++ src/main/java/frc/robot/commands/Intake/roll/IntakeRollCommand.java
@@ -0,0 +1,17 @@
+package frc.robot.commands.Intake.roll;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.IntakeRoll;
+
+public class IntakeRollCommand extends Command {
+    private final IntakeRoll roll;
+    public IntakeRollCommand(IntakeRoll roll){
+        this.roll = roll;
+        addRequirements(roll);
+    }
+
+    @Override
+    public void execute() {
+        roll.setSpeed(-0.5);
+    }
+}

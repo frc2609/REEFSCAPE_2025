e48f3c591e69ded917f5c6ecd9001430e5a6c3ba
billy <billy.parmenter98@gmail.com>
2025-03-18 19:33:43 -0400

    changed command to have default positions

--- src/main/java/frc/robot/commands/ScoreL4CommandAuto.java
+++ src/main/java/frc/robot/commands/ScoreL4CommandAuto.java
@@ -1,5 +1,7 @@
 package frc.robot.commands;
 
+import com.pathplanner.lib.auto.NamedCommands;
+
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
@@ -8,6 +10,7 @@ import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.commands.gripper.AutoReleaseGripperCommand;
 import frc.robot.commands.gripper.ReleaseGripperCommand;
+import frc.robot.commands.gripper.SlowGripCommand;
 import frc.robot.commands.pcmUtils.MovePCM;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Elevator;
@@ -16,6 +19,9 @@ import frc.robot.subsystems.Gripper;
 public class ScoreL4CommandAuto extends SequentialCommandGroup{
     public ScoreL4CommandAuto(Elevator elevator, Arm arm, Gripper gripper){
         addCommands(
+         new MovePCM(elevator, 8.5),
+         new MovePCM(arm, 0),
+         new SlowGripCommand(gripper),
             new ParallelCommandGroup(
             new MovePCM(elevator, 38), //39
             new SequentialCommandGroup(

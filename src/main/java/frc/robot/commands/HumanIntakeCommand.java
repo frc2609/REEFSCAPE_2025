01746a3100a319ef58233f6575e70a6519c01d87
UserC2 <aj01cars@outlook.com>
2025-03-21 13:12:06 -0400

    WORKS 3 AUTO

--- src/main/java/frc/robot/commands/HumanIntakeCommand.java
+++ src/main/java/frc/robot/commands/HumanIntakeCommand.java
@@ -17,7 +17,7 @@ public class HumanIntakeCommand extends SequentialCommandGroup {
             new GripCommand(gripper),
             new WaitUntilCommand(elevator.aboveIntake),
             new MovePCM(arm,-70 ),//-91
-            new MovePCM (elevator,12.5 ),//6
+            new MovePCM (elevator,14 ),//6
             new WaitCommand(100)
         );
     }

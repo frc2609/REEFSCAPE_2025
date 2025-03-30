60e02ec08d10be9548dd39cec2b9bfbe1a6585cf
billy <billy.parmenter98@gmail.com>
2025-03-30 19:09:59 -0400

    removed unused imports

--- src/main/java/frc/robot/commands/auto/MultiTagPathAuto.java
+++ src/main/java/frc/robot/commands/auto/MultiTagPathAuto.java
@@ -6,7 +6,6 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
